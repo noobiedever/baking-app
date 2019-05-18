3eacd6aa365320bdedb71fad8f93c968caf38c5a
Julius Smith <jsmit177@gmail.com>
2019-05-18 10:59:08 -0700

    Add MainActivityScreenTest

--- app/src/androidTest/java/com/example/bakingapp/MainActivityScreenTest.java
+++ app/src/androidTest/java/com/example/bakingapp/MainActivityScreenTest.java
@@ -7,21 +7,26 @@ import org.junit.runner.RunWith;
 
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
-
 @RunWith(AndroidJUnit4.class)
 public class MainActivityScreenTest {
+    private static String RECIPE_NAME = "Nutella Pie";
 
     @Rule
-    ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void clickRecyclerViewItem_OpensRecipeDetailActivity() {
-        Espresso.onData(Matchers.anything()).inAdapterView(ViewMatchers.withId(R.id.rv_recipe_list))
-                .atPosition(0).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.rv_recipe_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, ViewActions.click()));
+
+        Espresso.onView(ViewMatchers.withId(R.id.tv_recipe_name))
+                .check(ViewAssertions.matches(ViewMatchers.withText(RECIPE_NAME)));
     }
 }
